cc95f8e0f826028d389c4b39ec5be7542757156d
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-12 14:57:22 +0500

    infosys_website_clone

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -35,7 +35,7 @@ const slides = [
 
 const Home = () => {
   return (
-    <section className="carousel slide carousel-fade" data-bs-ride="carousel" id="slider1" data-bs-interval="5000">
+    <section className="carousel slide carousel-fade" data-bs-ride="carousel" id="slider1" data-bs-interval="3000">
       <div className="carousel-indicators">
         {slides.map((_, i) => (
           <button
