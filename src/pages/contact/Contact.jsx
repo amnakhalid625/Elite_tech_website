e06f1dd5c4a2414333746a46fe8d710725d9f331
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-12 21:42:28 +0500

    infosys_website_clone

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,30 @@
 import './contact.css';
 
 const ContactSection = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "00a22c40-b7c7-4daf-9e9f-dbc47dc0d9e0");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
+
+
   return (
     <section className="contact-section" id="contact">
       <div className="container contact-container">
@@ -9,11 +33,11 @@ const ContactSection = () => {
           <p className="section-subtitle">We’d love to hear from you. Fill in the form below and we’ll get back to you shortly.</p>
         </div>
 
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={onSubmit}>
           <div className="form-group-grid">
-            <input type="text" placeholder="Your Name" required />
-            <input type="email" placeholder="Your Email" required />
-            <input type="text" placeholder="Company Name" />
+            <input type="text" placeholder="Your Name" name='name' required />
+            <input type="email" placeholder="Your Email" name='email' required />
+            <input type="text" placeholder="Company Name" name='company' />
             <select>
               <option value="">Budget Range (Optional)</option>
               <option value="1k-5k">100pkr - 500</option>
@@ -28,7 +52,7 @@ const ContactSection = () => {
 
             </select>
           </div>
-          <textarea rows="5" placeholder="Tell us about your project..."></textarea>
+          <textarea rows="5" placeholder="Tell us about your project..." name='message'></textarea>
           <div className="text-center">
             <button type="submit" className="submit-btn">Submit</button>
           </div>
