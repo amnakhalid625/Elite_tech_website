cc582300854cf1726437d1b90baf91411aee1314
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-14 19:10:47 +0500

    elite_website

--- src/pages/bell/bell.jsx
+++ src/pages/bell/bell.jsx
@@ -7,7 +7,7 @@ const Bell = () => {
         <h4 className='bell-heading'>Technology that Works for You</h4>
         <h5 className='bell-subheading'>"Enhancing Productivity and Streamlining Operations with Smart Solutions"</h5>
         
-        <a >
+        <a href='https://blog.bit.ai/top-technology-blogs' target='_blank' >
           <button className='bell-button'>READ MORE</button>
         </a>
       </div>
