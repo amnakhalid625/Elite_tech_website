017633d57e64e131daad86ba2aab1a213d355f9c
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-15 17:22:00 +0500

    elite_website

--- src/pages/testimonial/Testimonial.jsx
+++ src/pages/testimonial/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './testimonial.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
