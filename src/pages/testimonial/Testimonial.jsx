d9ce390cd28f8d34fadc35d11a8ed572adfee6bb
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-15 22:04:12 +0500

    elite_website

--- src/pages/testimonial/Testimonial.jsx
+++ src/pages/testimonial/Testimonial.jsx
@@ -4,7 +4,6 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css/autoplay';
-
 import { collection, addDoc, onSnapshot } from "firebase/firestore";
 import db from "../../firebase/firebaseConfig";
 import { useEffect, useState } from 'react';
